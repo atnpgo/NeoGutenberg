ddf7b85174565073a052c626d56512669a20d230
Etienne Pageau <etienne.pageau@gmail.com>
2017-08-31 20:08:02 -0400

    conversion

--- main.js
+++ main.js
@@ -1,57 +1,128 @@
+/* global __dirname */
+
 const electron = require('electron');
-// Module to control application life.
 const app = electron.app;
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
+const fs = require('fs');
 const path = require('path');
 const url = require('url');
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-const createWindow = () => {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+const Epub = require("epub-gen");
+const kindlegen = require("kindlegen");
+const showdown = require('showdown');
+const converter = new showdown.Converter({
+    strikethrough: true,
+    tables: true
+});
 
-    // and load the index.html of the app.
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-    
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+const neogut = {
+    /**
+     * global reference of the window object
+     * @type BrowserWindow
+     */
+    mainWindow: null,
+    /**
+     * The base path
+     * @type String
+     */
+    basePath: null,
+    /**
+     * Creates a window
+     * @returns {undefined}
+     */
+    createWindow: () => {
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
+        neogut.basePath = path.join(app.getPath('documents'), 'NeoGutenberg');
+        // Create the browser window.
+        neogut.mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600
+        });
+
+        // and load the index.html of the app.
+        neogut.mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+
+        // Open the DevTools.
+        neogut.mainWindow.webContents.openDevTools()
+
+        // Emitted when the window is closed.
+        neogut.mainWindow.on('closed', () => {
+            // when you should delete the corresponding element.
+            neogut.mainWindow = null;
+        });
+    },
+    generateBook: (title, callback) => {
+        const content = [];
+        const bookFolder = path.join(neogut.basePath, title);
+        fs.readdir(bookFolder, (err, files) => {
+            callback("Building content");
+            files.forEach((file) => {
+                const fullPath = path.join(bookFolder, file);
+                const baseName = path.basename(file, '.md');
+                if (!fs.lstatSync(fullPath).isDirectory() && !path.basename(file).startsWith('_')) {
+                    callback("Building Chapter " + baseName);
+                    content.push({
+                        title: baseName.substring(2),
+                        data: converter.makeHtml(fs.readFileSync(fullPath, 'UTF-8'))
+                    });
+                }
+            });
+
+            callback("Building ePub");
+            const coverPath = path.join(path.join(bookFolder, '_assets'), 'cover.jpg');
+            const epubPath = path.join(path.join(bookFolder, '_out'), title + '.epub');
+            const mobiPath = path.join(path.join(bookFolder, '_out'), title + '.mobi');
+
+            new Epub({
+                output: epubPath,
+                title,
+                content,
+                author: 'ATNPGO',
+                publisher: 'NeoGutenberg',
+                cover: fs.existsSync(coverPath) ? coverPath : null
+            }).promise.then(function () {
+                callback("Building mobi");
+                kindlegen(fs.readFileSync(epubPath), (error, mobi) => {
+                    callback("Building mobi");
+                    fs.writeFile(mobiPath, mobi, (err) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback("Ebooks Generated Successfully!");
+                        }
+                    });
+                });
+            }, (err) => {
+                callback("Failed to generate epub because of " + err);
+            });
+
+
+
+        });
+    }
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+
+app.on('ready', neogut.createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        // wait for cmd + Q
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow();
     }
-})
+});
+
+exports.test = neogut.generateBook;
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
