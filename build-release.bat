f31c31c843f757d0c710531fea048b8119091a73
ATNPGO <etienne.pageau@gmail.com>
2017-09-04 15:34:05 -0400

    Release files.

--- build-release.bat
+++ build-release.bat
@@ -1,6 +1,8 @@
+@RD /S /Q dist & ^
+npm install & ^
 npm run package-win & ^
+npm run package-linux & ^
 npm run create-installer-win-x64 & ^
-npm run create-installer-win-ia32 & ^
-npm run package-linux
+npm run create-installer-win-ia32
 REM npm run package-mac & ^
 REM npm run create-installer-mac
