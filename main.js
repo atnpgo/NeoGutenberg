c1eb1a1f3bd198874a7d24370524f79ee0af6c8a
ATNPGO <etienne.pageau@gmail.com>
2017-09-04 19:13:37 -0400

    Added missing setup script.

--- main.js
+++ main.js
@@ -12,6 +12,14 @@
 
 /* global __dirname, Promise */
 
+//handle setupevents as quickly as possible
+const setupEvents = require('./installers/setupEvents');
+if (setupEvents.handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
+
+
 const electron = require('electron');
 const app = electron.app;
 const dialog = electron.dialog;
