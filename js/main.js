4dd271785b6c88897a04cd1dbe7cddad968e6000
ATNPGO <etienne.pageau@gmail.com>
2019-06-08 12:33:01 -0400

    Code cleanup, version bump.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-
 function getOS() {
     var userAgent = window.navigator.userAgent,
-            platform = window.navigator.platform,
-            macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-            windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-            iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-            os = null;
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
     if (macosPlatforms.indexOf(platform) !== -1) {
         os = 'Mac OS';
     } else if (iosPlatforms.indexOf(platform) !== -1) {
@@ -23,28 +22,28 @@ function getOS() {
 
 $(document).ready(function (e) {
     $('#btn-download-now').click(function () {
-        var distFolder = 'https://raw.githubusercontent.com/atnpgo/NeoGutenberg/master/dist/';
+        var distFolder = 'dist/';
         switch (getOS()) {
-            case 'Windows':
-                if (navigator.userAgent.indexOf('64') !== -1) {
-                    window.location = distFolder + 'NeoGutenbergInstaller.x64.exe';
-                } else {
-                    window.location = distFolder + 'NeoGutenbergInstaller.ia32.exe';
-                }
-                break;
             case 'Mac OS':
-                window.location = distFolder + 'NeoGutenberg.dmg';
+                window.location = distFolder + 'NeoGutenberg-0.1.1.dmg';
                 break;
             case 'Linux':
                 if (navigator.userAgent.indexOf('64') !== -1) {
-                    window.location = distFolder + 'NeoGutenberg-linux-x64.zip';
+                    window.location = distFolder + 'NeoGutenberg-0.1.1.AppImage';
                 } else if (navigator.userAgent.indexOf('32') !== -1) {
                     window.location = distFolder + 'NeoGutenberg-linux-ia32.zip';
                 } else {
                     window.location = distFolder + 'NeoGutenberg-linux-armv7l.zip';
                 }
                 break;
+            case 'Windows':
             default:
+                if (navigator.userAgent.indexOf('64') !== -1) {
+                    window.location = distFolder + 'NeoGutenbergInstaller.x64.exe';
+                } else {
+                    window.location = distFolder + 'NeoGutenbergInstaller.ia32.exe';
+                }
+                break;
 
         }
     });
